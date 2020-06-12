070f169ff9ed602f2e235f6f4cc4b5f556010702
Rino Reji Cheriyan <Rino.RejiCheriyan@gds.ey.com>
2020-06-13 02:06:38 +0530

    intro reactive forms

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl } from '@angular/forms';
+import { Observable } from 'rxjs';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Energy meter';
+  formGrp: FormGroup;
+
+  cost$: Observable<any>;
+
+  constructor() {
+    this.initForm();
+  }
+
+  private initForm() {
+    this.formGrp = new FormGroup({
+      units: new FormControl('')
+    });
+    this.cost$ = this.formGrp.get('units').valueChanges;
+    this.formGrp.get('units').valueChanges.subscribe(v=> {
+      console.log(v);
+    });
+  }
 }
